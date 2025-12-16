03b839a27f8f0601ddc54bc203d0ecea29b0698a
adz610 <adz610@mail.ru>
2025-12-16 17:53:29 +0300

    add new branch with Lesson_2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -29,7 +29,7 @@ public class Main {
         System.out.println("Аттракционы парка \"" + park.getName() + "\":");
         for (Park.Attraction attraction : attractions) {
             attraction.getInfo();
-            System.out.println("1");
+            System.out.println();
         }
     }
 }
