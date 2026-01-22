980999c036640c0b796a4cd4fdf48994d8ae1f73
adz610 <adz610@mail.ru>
2026-01-22 22:29:46 +0300

    add new branch with Lesson_9

---
+++ src/main/java/org/example/HomePage.java
@@ -0,0 +1,224 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Objects;
+
+public class HomePage {
+    protected final WebDriver driver;
+    protected final WebDriverWait wait;
+
+    // payment block
+    private final By PAYMENT_BLOCK = By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section");
+    private final By TITLE = By.xpath("div/h2");
+    private final By PARTNER_LOGOS = By.xpath("div/div[@class=\"pay__partners\"]");
+    private final By ABOUT_SERVICE_BUTTON = By.xpath("div/a");
+    private final By TAB_SELECT_BUTTON = By.xpath("//button[@class=\"select__header\"]");
+    private final By OPENED_PAY_FORM = By.xpath("//form[@class=\"pay-form opened\"]");
+    private final By NUMBER_FIELD = By.xpath("div[1]/input");
+    private final By SUM_FIELD = By.xpath("div[2]/input");
+    private final By EMAIL_FIELD = By.xpath("div[3]/input");
+
+    // payment form (iframe)
+    private final By PAYMENT_FORM = By.xpath("//iframe[@class=\"payment-widget-iframe\"]");
+    private final By DESCRIPTION = By.xpath("//*[@class=\"pay-description__text\"]");
+    private final By SUM = By.xpath("//*[@class=\"pay-description__cost\"]");
+    private final By SUBMIT_BUTTON = By.xpath("//button[@type=\"submit\"]");
+    private final By CARD_NUMBER_LABEL = By.xpath("//input[@formcontrolname=\"creditCard\"]/following-sibling::label");
+    private final By CARD_EXPIRATION_DATE_LABEL = By.xpath("//input[@formcontrolname=\"expirationDate\"]/following-sibling::label");
+    private final By CARD_CVC_LABEL = By.xpath("//input[@formcontrolname=\"cvc\"]/following-sibling::label");
+    private final By CARD_HOLDER_LABEL = By.xpath("//input[@formcontrolname=\"holder\"]/following-sibling::label");
+    private final By CARD_PARTNER_LOGOS = By.xpath("//div[contains(@class, \"cards-brands__container\")]");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void open() {
+        driver.get("https://www.mts.by/");
+    }
+
+    public void acceptCookies() {
+        wait.until(
+            ExpectedConditions.elementToBeClickable(
+                driver.findElement(By.cssSelector("#cookie-agree"))
+            )
+        ).click();
+    }
+
+    public String getPaymentBlockTitleText() {
+        return driver.findElement(PAYMENT_BLOCK)
+            .findElement(TITLE)
+            .getText();
+    }
+
+    public List<WebElement> getPartnerLogos() {
+        return driver.findElement(PAYMENT_BLOCK)
+            .findElement(PARTNER_LOGOS)
+            .findElements(By.tagName("img"));
+    }
+
+    public WebElement getContinueButton() {
+        WebElement button = driver.findElement(PAYMENT_BLOCK)
+            .findElement(OPENED_PAY_FORM)
+            .findElement(By.xpath("button"));
+        return wait.until(
+            ExpectedConditions.visibilityOf(button)
+        );
+    }
+
+    public void clickAboutServiceButton() {
+        WebElement button = driver.findElement(PAYMENT_BLOCK).findElement(ABOUT_SERVICE_BUTTON);
+        wait.until(
+            ExpectedConditions.elementToBeClickable(button)
+        ).click();
+    }
+
+    public void fillPhoneNumberField(String phoneNumber) {
+        WebElement field = driver.findElement(PAYMENT_BLOCK)
+            .findElement(OPENED_PAY_FORM)
+            .findElement(NUMBER_FIELD);
+        field.clear();
+        field.sendKeys(phoneNumber);
+    }
+
+    public void fillSumField(String sum) {
+        WebElement field = driver.findElement(PAYMENT_BLOCK)
+            .findElement(OPENED_PAY_FORM)
+            .findElement(SUM_FIELD);
+        field.clear();
+        field.sendKeys(sum);
+    }
+
+    public void clickContinueButton() {
+        wait.until(
+            ExpectedConditions.elementToBeClickable(
+                getContinueButton()
+            )
+        ).click();
+    }
+
+    public void switchToPaymentForm() {
+        driver.switchTo().frame(
+            wait.until(
+                ExpectedConditions.visibilityOfElementLocated(PAYMENT_FORM)
+            )
+        );
+    }
+
+    public String getPaymentFormDescriptionText() {
+        return Objects.requireNonNull(
+            wait.until(
+                ExpectedConditions.visibilityOfElementLocated(DESCRIPTION)
+            )
+        ).getText();
+    }
+
+    public void selectPaymentTab (int id) {
+        WebElement button = driver.findElement(PAYMENT_BLOCK).findElement(TAB_SELECT_BUTTON);
+        wait.until(
+            ExpectedConditions.elementToBeClickable(button)
+        ).click();
+
+        WebElement tab = driver.findElement(PAYMENT_BLOCK).findElement(By.xpath(String.format(".//li[%d]", id)));
+        wait.until(
+            ExpectedConditions.elementToBeClickable(tab)
+        ).click();
+    }
+
+    public String getNumberFieldPlaceholder() {
+        WebElement field = driver.findElement(PAYMENT_BLOCK)
+            .findElement(OPENED_PAY_FORM)
+            .findElement(NUMBER_FIELD);
+        return Objects.requireNonNull(
+            wait.until(
+                ExpectedConditions.visibilityOf(field)
+            )
+        ).getAttribute("placeholder");
+    }
+
+    public String getSumFieldPlaceholder() {
+        WebElement field = driver.findElement(PAYMENT_BLOCK)
+            .findElement(OPENED_PAY_FORM)
+            .findElement(SUM_FIELD);
+        return Objects.requireNonNull(
+            wait.until(
+                ExpectedConditions.visibilityOf(field)
+            )
+        ).getAttribute("placeholder");
+    }
+
+    public String getEmailFieldPlaceholder() {
+        WebElement field = driver.findElement(PAYMENT_BLOCK)
+            .findElement(OPENED_PAY_FORM)
+            .findElement(EMAIL_FIELD);
+        return Objects.requireNonNull(
+            wait.until(
+                ExpectedConditions.visibilityOf(field)
+            )
+        ).getAttribute("placeholder");
+    }
+
+    public String getPaymentFormSumText() {
+        return Objects.requireNonNull(
+            wait.until(
+                ExpectedConditions.visibilityOfElementLocated(SUM)
+            )
+        ).getText();
+    }
+
+    public String getPaymentFormSubmitButtonText() {
+        return Objects.requireNonNull(
+            wait.until(
+                ExpectedConditions.visibilityOfElementLocated(SUBMIT_BUTTON)
+            )
+        ).getText();
+    }
+
+    public String getPaymentFormCardNumberLabel() {
+        return Objects.requireNonNull(
+            wait.until(
+                ExpectedConditions.visibilityOfElementLocated(CARD_NUMBER_LABEL)
+            )
+        ).getText();
+    }
+
+    public String getPaymentFormCardExpirationDateLabel() {
+        return Objects.requireNonNull(
+            wait.until(
+                ExpectedConditions.visibilityOfElementLocated(CARD_EXPIRATION_DATE_LABEL)
+            )
+        ).getText();
+    }
+
+    public String getPaymentFormCardCVCLabel() {
+        return Objects.requireNonNull(
+            wait.until(
+                ExpectedConditions.visibilityOfElementLocated(CARD_CVC_LABEL)
+            )
+        ).getText();
+    }
+
+    public String getPaymentFormCardHolderLabel() {
+        return Objects.requireNonNull(
+            wait.until(
+                ExpectedConditions.visibilityOfElementLocated(CARD_HOLDER_LABEL)
+            )
+        ).getText();
+    }
+
+    public List<WebElement> getPaymentFormPartnerLogos() {
+        WebElement container = wait.until(
+            ExpectedConditions.visibilityOfElementLocated(CARD_PARTNER_LOGOS)
+        );
+        return container.findElements(By.tagName("img"));
+    }
+}
--- src/test/java/PaymentBlockTest.java
+++ src/test/java/PaymentBlockTest.java
@@ -1,7 +1,5 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.example.HomePage;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,23 +7,20 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PaymentBlockTest {
     private WebDriver driver;
-    private WebDriverWait wait;
+    private HomePage homePage;
 
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.get("https://www.mts.by/");
-        WebElement cookieAcceptButton = driver.findElement(By.xpath("//*[@id=\"cookie-agree\"]"));
-        wait.until(ExpectedConditions.elementToBeClickable(cookieAcceptButton));
-        cookieAcceptButton.click();
+        homePage = new HomePage(driver);
+        homePage.open();
+        homePage.acceptCookies();
     }
 
     @AfterEach
@@ -35,45 +30,81 @@ public class PaymentBlockTest {
 
     @Test
     void checkBlockTitle() {
-        WebElement title = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/h2"));
-        assertEquals("Онлайн пополнение\nбез комиссии", title.getText());
+        assertEquals("Онлайн пополнение\nбез комиссии", homePage.getPaymentBlockTitleText());
     }
 
     @Test
     void checkPartnerLogos() {
-        WebElement logosContainer = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]"));
-        List<WebElement> logos = logosContainer.findElements((By.tagName("img")));
-        for (WebElement logo : logos) {
+        for (WebElement logo : homePage.getPartnerLogos()) {
             assertTrue(logo.isDisplayed());
         }
     }
 
     @Test
     void aboutServiceButtonTest() {
-        WebElement button = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/a"));
-        wait.until(ExpectedConditions.elementToBeClickable(button));
-        button.click();
-        assertTrue(driver.getCurrentUrl().contains("help/poryadok-oplaty-i-bezopasnost-internet-platezhey"));
+        homePage.clickAboutServiceButton();
+        assertTrue(Objects.requireNonNull(driver.getCurrentUrl()).contains("help/poryadok-oplaty-i-bezopasnost-internet-platezhey"));
     }
 
     @Test
     void continueButtonTest() {
         String phoneNumber = "297777777";
-        WebElement phoneNumberField = driver.findElement(By.xpath("//*[@id=\"connection-phone\"]"));
-        phoneNumberField.sendKeys(phoneNumber);
-
-        WebElement sumField = driver.findElement(By.xpath("//*[@id=\"connection-sum\"]"));
-        sumField.sendKeys("228");
-
-        WebElement continueButton = driver.findElement(By.xpath("//*[@id=\"pay-connection\"]/button"));
-        assertTrue(continueButton.isEnabled());
-        continueButton.click();
-
-        WebElement paymentForm = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[9]/div/iframe")));
-        driver.switchTo().frame(paymentForm);
-
-        WebElement paymentFormDescription = driver.findElement(By.xpath("/html/body/app-root/div/div/div/app-payment-container/section/div/div/div[2]"));
         String expectedDescription = "Оплата: Услуги связи Номер:375" + phoneNumber;
-        assertEquals(expectedDescription, paymentFormDescription.getText());
+
+        homePage.fillPhoneNumberField(phoneNumber);
+        homePage.fillSumField("228");
+        assertTrue(homePage.getContinueButton().isEnabled());
+        homePage.clickContinueButton();
+        homePage.switchToPaymentForm();
+        assertEquals(expectedDescription, homePage.getPaymentFormDescriptionText());
+    }
+
+    @Test
+    void checkPlaceholders() {
+        for (int i = 1; i <= 4; i++) {
+            homePage.selectPaymentTab(i);
+            switch (i) {
+                case 1:
+                    assertEquals("Номер телефона", homePage.getNumberFieldPlaceholder());
+                    break;
+                case 2:
+                    assertEquals("Номер абонента", homePage.getNumberFieldPlaceholder());
+                    break;
+                case 3:
+                    assertEquals("Номер счета на 44", homePage.getNumberFieldPlaceholder());
+                    break;
+                case 4:
+                    assertEquals("Номер счета на 2073", homePage.getNumberFieldPlaceholder());
+                    break;
+            }
+            assertEquals("Сумма", homePage.getSumFieldPlaceholder());
+            assertEquals("E-mail для отправки чека", homePage.getEmailFieldPlaceholder());
+        }
+    }
+
+    @Test
+    void checkPaymentForm() {
+        String phoneNumber = "297777777";
+        String sum = "249.99";
+
+        homePage.fillPhoneNumberField(phoneNumber);
+        homePage.fillSumField(sum);
+        homePage.clickContinueButton();
+        homePage.switchToPaymentForm();
+
+        assertEquals(String.format("%s BYN", sum), homePage.getPaymentFormSumText());
+        assertEquals(String.format("Оплатить %s BYN", sum), homePage.getPaymentFormSubmitButtonText());
+        assertEquals(String.format("Оплата: Услуги связи Номер:375%s", phoneNumber), homePage.getPaymentFormDescriptionText());
+
+        assertEquals("Номер карты", homePage.getPaymentFormCardNumberLabel());
+        assertEquals("Срок действия", homePage.getPaymentFormCardExpirationDateLabel());
+        assertEquals("CVC", homePage.getPaymentFormCardCVCLabel());
+        assertEquals("Имя и фамилия на карте", homePage.getPaymentFormCardHolderLabel());
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        for (WebElement logo : homePage.getPaymentFormPartnerLogos()) {
+            wait.until(ExpectedConditions.visibilityOf(logo));
+            assertTrue(logo.isDisplayed());
+        }
     }
 }
